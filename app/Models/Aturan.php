5f0ca82e545e1a2d501c68fa133d4e8680d8fd16
Ahmad Mathlaul Falah <ahmadmathlaulfalah14@gmail.com>
2026-06-10 21:39:48 +0700

    Update Aturan model

--- app/Models/Aturan.php
+++ app/Models/Aturan.php
@@ -12,7 +12,24 @@ class Aturan extends Model
     use SoftDeletes;
 
     protected $table = 'aturan';
-    protected $fillable = ['kode', 'diagnosa_id', 'cf_pakar', 'prioritas', 'is_active', 'deskripsi', 'min_threshold'];
+
+    protected $fillable = [
+        'kode',
+        'diagnosa_id',
+        'cf_pakar',
+        'prioritas',
+        'is_active',
+        'deskripsi',
+        'min_threshold',
+    ];
+
+    protected $casts = [
+        'diagnosa_id' => 'integer',
+        'cf_pakar' => 'float',
+        'prioritas' => 'integer',
+        'is_active' => 'boolean',
+        'min_threshold' => 'float',
+    ];
 
     public function diagnosa(): BelongsTo
     {
@@ -21,6 +38,7 @@ class Aturan extends Model
 
     public function gejala(): BelongsToMany
     {
-        return $this->belongsToMany(Gejala::class, 'aturan_gejala', 'aturan_id', 'gejala_id');
+        return $this->belongsToMany(Gejala::class, 'aturan_gejala', 'aturan_id', 'gejala_id')
+            ->withPivot(['bobot_pakar', 'evidence_direction']);
     }
 }
