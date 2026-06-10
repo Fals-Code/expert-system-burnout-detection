cc080e50f86711a088e1e61316b87a57f483fe65
Ahmad Mathlaul Falah <ahmadmathlaulfalah14@gmail.com>
2026-06-11 01:35:21 +0700

    Use Sanctuary Hub SVG favicon on auth pages

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Sanctuary Hub')</title>
     
-    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 24 24%22 fill=%22none%22 stroke=%22%232563eb%22 stroke-width=%222.5%22 stroke-linecap=%22round%22 stroke-linejoin=%22round%22><path d=%22M9 11l3 3L22 4%22></path><path d=%22M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11%22></path></svg>">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('assets/img/sanctuary-hub-mark.svg') }}">
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
