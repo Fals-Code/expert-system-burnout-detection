8c06e26d26a8a676e4647f033a8d31a48c883326
Ahmad Mathlaul Falah <ahmadmathlaulfalah14@gmail.com>
2026-06-11 11:07:22 +0700

    feat: add CBI work item definitions

--- database/seeders/data/cbi_work.php
+++ database/seeders/data/cbi_work.php
@@ -1,3 +1,11 @@
 <?php
 
-return [];
+return [
+    ['CBI-WB-01', 'Seberapa sering pekerjaan Anda terasa menguras emosi?', false],
+    ['CBI-WB-02', 'Seberapa sering Anda merasa kelelahan karena pekerjaan Anda?', false],
+    ['CBI-WB-03', 'Seberapa sering pekerjaan Anda membuat Anda frustrasi?', false],
+    ['CBI-WB-04', 'Seberapa sering Anda merasa sangat letih pada akhir hari kerja?', false],
+    ['CBI-WB-05', 'Seberapa sering Anda merasa lelah pada pagi hari ketika memikirkan harus menghadapi hari kerja berikutnya?', false],
+    ['CBI-WB-06', 'Seberapa sering Anda merasa setiap jam kerja terasa melelahkan?', false],
+    ['CBI-WB-07', 'Seberapa sering Anda memiliki cukup energi untuk keluarga dan teman selama waktu luang?', true],
+];
