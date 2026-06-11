d7db806628136b86919b50ba23d5b3ba35781103
Ahmad Mathlaul Falah <ahmadmathlaulfalah14@gmail.com>
2026-06-11 11:37:02 +0700

    feat: define recursive inference statuses

--- app/ExpertSystem/InferenceStatus.php
+++ app/ExpertSystem/InferenceStatus.php
@@ -4,4 +4,9 @@ namespace App\ExpertSystem;
 
 enum InferenceStatus: string
 {
+    case NEED_FACT = 'NEED_FACT';
+    case PROVEN = 'PROVEN';
+    case REJECTED = 'REJECTED';
+    case EXHAUSTED = 'EXHAUSTED';
+    case LOOP_DETECTED = 'LOOP_DETECTED';
 }
